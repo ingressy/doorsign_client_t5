630ac30f6938c4554dbe298c7da6c15592d71935
ingressy <j.czinzoll@ingressy.de>
2025-09-11 19:50:23 +0200

    Add Wifi Support

--- client.ino
+++ client.ino
@@ -1,9 +1,33 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
 
+//ssid and password for wifi
+const char* ssid = "";
+const char* password = "";
+
+void setup() {
+  Serial.begin(115200);
+  connectwifi();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+}
+
+void connectwifi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  Serial.println("Connecting to WiFi Network ...");
+
+  //wait antill wifi is connected 
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(100);
+  }
+
+  Serial.println("Connected to the " + String(ssid) + " network");
+  Serial.println("IP:" + WiFi.localIP().toString());
+
 
 }
