c2795975026966095ef82a4329b011b0dd8418ba
ingressy <j.czinzoll@ingressy.de>
2025-09-11 20:38:45 +0200

    Add EEPROM for Longtime Saving

--- client.ino
+++ client.ino
@@ -1,11 +1,22 @@
 #include <WiFi.h>
+#include <EEPROM.h>
+
+//512 Byte | one letter a Byte
+#define EEPROM_SIZE 512
+
+//EEPROM info | NAMEadd = address | NAMEdata = data || address equals letter  || COUNT LETTERS!!!
+const int ssidadd = 0;
+const int passadd = 20;
 
 //ssid and password for wifi
-const char* ssid = "";
-const char* password = "";
+String ssid = "";
+String password = "";
 
 void setup() {
+  //begin things
   Serial.begin(115200);
+  Serial.println("Start DoorSign Client ...");
+  readEEPROM();
   connectwifi();
 }
 
@@ -26,8 +37,31 @@ void connectwifi() {
     delay(100);
   }
 
-  Serial.println("Connected to the " + String(ssid) + " network");
+  Serial.println("\nConnected to the " + String(ssid) + " network");
   Serial.println("IP:" + WiFi.localIP().toString());
 
+}
 
+void readEEPROM() {
+  Serial.println("Begin to read EEPROM ...");
+
+  EEPROM.begin(EEPROM_SIZE);
+
+  //check if EEPROM Address is initialized || didnt check corruption
+  if (EEPROM.read(ssidadd) != 0xFF) {
+    ssid = EEPROM.readString(ssidadd);
+    Serial.println("Found SSID Data in EEPROM ...");
+  } else {
+    Serial.println("Didnt find SSID Data in EEPROM!");
+    loop();
+  }
+  if (EEPROM.read(passadd) != 0xFF) {
+    password = EEPROM.readString(passadd);
+    Serial.println("Found Password Data in EEPROM ...");
+  } else {
+    Serial.println("Didnt find Password Data in EEPROM!");
+    loop();
+  }
+  
+  Serial.println("End to read EEPROM ...");
 }
