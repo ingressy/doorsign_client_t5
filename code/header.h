589ca234db872dfb16d1a0819ea3d328150ef8bd
Noora Liv <j.czinzoll@ingressy.de>
2025-09-27 23:22:01 +0200

    Update header.h

--- code/header.h
+++ code/header.h
@@ -1,7 +1,19 @@
-#ifndef HEADER_H
-    #define HEADER_H
+#pragma once //ruft die datei nur einmal auf
+extern String ssid, password, IP, MAC;
+extern uint8_t *framebuffer;
+extern int id;
 
-    //Declaration
-    void readEEPROM();
-    void eeprominfo();
-#endif
+extern struct tm timeinfo;
+extern char time_str[32];
+
+
+//Declaration
+void readEEPROM();
+bool readRoom();
+
+void eeprominfo();
+
+void errormenu(int menu);
+
+void connectwifi();
+void rtctimeset();
